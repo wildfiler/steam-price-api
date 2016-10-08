81992c0c3d7d5026e0f2560723bbf85cd3bdfe5f
Alexey Neischenko <alexneisc@gmail.com>
2016-10-08 18:53:11 +0300

    Replace where by find_by

--- app/jobs/steam_market_item_nameid_import_job.rb
+++ app/jobs/steam_market_item_nameid_import_job.rb
@@ -3,7 +3,7 @@ class SteamMarketItemNameidImportJob
   sidekiq_options queue: :import_item_nameid
 
   def perform(app_id, item_name, item_nameid)
-    item = SteamMarketItem.where(app_id: app_id, name: item_name)
+    item = SteamMarketItem.find_by(app_id: app_id, name: item_name)
     raise "Item not found" unless item
     item.update(item_nameid: item_nameid)
   end
