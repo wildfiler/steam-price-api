5c04b01682ed026ba35784f9e470df01f029cc37
Alexey Neischenko <alexneisc@gmail.com>
2016-10-08 18:51:50 +0300

    Add raise to SteamMarketItemNameidImportJob

--- app/jobs/steam_market_item_nameid_import_job.rb
+++ app/jobs/steam_market_item_nameid_import_job.rb
@@ -3,6 +3,8 @@ class SteamMarketItemNameidImportJob
   sidekiq_options queue: :import_item_nameid
 
   def perform(app_id, item_name, item_nameid)
-    SteamMarketItem.find_by(app_id: app_id, name: item_name).update(item_nameid: item_nameid)
+    item = SteamMarketItem.where(app_id: app_id, name: item_name)
+    raise "Item not found" unless item
+    item.update(item_nameid: item_nameid)
   end
 end
