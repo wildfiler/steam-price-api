5b133119b9ab921522a09d45307a3d4c4fb61a77
Alexey Neischenko <alexneisc@gmail.com>
2016-11-06 19:36:55 +0200

    Change type from decimal to float for moneys fields

--- db/migrate/20161106155803_create_steam_market_orders_details.rb
+++ db/migrate/20161106155803_create_steam_market_orders_details.rb
@@ -2,8 +2,8 @@ class CreateSteamMarketOrdersDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :steam_market_orders_details do |t|
       t.string :item_nameid, null: false, index: true
-      t.decimal :highest_buy_order, default: 0
-      t.decimal :lowest_sell_order, default: 0
+      t.float :highest_buy_order, default: 0
+      t.float :lowest_sell_order, default: 0
       t.integer :buy_order_summary, default: 0
       t.integer :sell_order_summary, default: 0
 
