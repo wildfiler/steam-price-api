367edbc2981417772663175675657b99e9fc0733
Alexey Neischenko <alexneisc@gmail.com>
2016-11-13 16:19:52 +0200

    Use hash instead 4 parameters in SteamMarketOrdersDetailsImportJob

--- app/jobs/steam_market_orders_details_import_job.rb
+++ app/jobs/steam_market_orders_details_import_job.rb
@@ -4,19 +4,16 @@ class SteamMarketOrdersDetailsImportJob
 
   def perform(
     item_nameid,
-    highest_buy_order,
-    lowest_sell_order,
-    buy_order_number,
-    sell_order_number,
+    steam_data,
     date_time
   )
 
     SteamMarketOrdersDetail.create!(
       item_nameid: item_nameid,
-      highest_buy_order: highest_buy_order.to_f/100,
-      lowest_sell_order: lowest_sell_order.to_f/100,
-      buy_order_number: buy_order_number,
-      sell_order_number: sell_order_number,
+      highest_buy_order: steam_data['highest_buy_order'].to_f/100,
+      lowest_sell_order: steam_data['lowest_sell_order'].to_f/100,
+      buy_order_number: steam_data['buy_order_number'],
+      sell_order_number: steam_data['sell_order_number'],
       created_at: date_time
     )
   end
